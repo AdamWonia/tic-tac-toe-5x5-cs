a837a5cfa9b9b11868ea5bffa7f921ade0f7ecab
Adam Wonia <adam.wonia@gmail.com>
2021-11-26 14:41:44 +0100

    images and game look added

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -29,13 +29,338 @@ namespace TicTacToe
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.box1 = new System.Windows.Forms.Button();
+            this.box2 = new System.Windows.Forms.Button();
+            this.box3 = new System.Windows.Forms.Button();
+            this.box4 = new System.Windows.Forms.Button();
+            this.box5 = new System.Windows.Forms.Button();
+            this.box6 = new System.Windows.Forms.Button();
+            this.box7 = new System.Windows.Forms.Button();
+            this.box8 = new System.Windows.Forms.Button();
+            this.box9 = new System.Windows.Forms.Button();
+            this.box10 = new System.Windows.Forms.Button();
+            this.box11 = new System.Windows.Forms.Button();
+            this.box16 = new System.Windows.Forms.Button();
+            this.box21 = new System.Windows.Forms.Button();
+            this.box12 = new System.Windows.Forms.Button();
+            this.box13 = new System.Windows.Forms.Button();
+            this.box14 = new System.Windows.Forms.Button();
+            this.box15 = new System.Windows.Forms.Button();
+            this.box17 = new System.Windows.Forms.Button();
+            this.box18 = new System.Windows.Forms.Button();
+            this.box19 = new System.Windows.Forms.Button();
+            this.box20 = new System.Windows.Forms.Button();
+            this.box22 = new System.Windows.Forms.Button();
+            this.box23 = new System.Windows.Forms.Button();
+            this.box24 = new System.Windows.Forms.Button();
+            this.box25 = new System.Windows.Forms.Button();
+            this.header = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // box1
+            // 
+            this.box1.Image = ((System.Drawing.Image)(resources.GetObject("box1.Image")));
+            this.box1.Location = new System.Drawing.Point(231, 112);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(60, 60);
+            this.box1.TabIndex = 0;
+            this.box1.UseVisualStyleBackColor = true;
+            // 
+            // box2
+            // 
+            this.box2.Image = ((System.Drawing.Image)(resources.GetObject("box2.Image")));
+            this.box2.Location = new System.Drawing.Point(297, 112);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(60, 60);
+            this.box2.TabIndex = 1;
+            this.box2.UseVisualStyleBackColor = true;
+            // 
+            // box3
+            // 
+            this.box3.Image = ((System.Drawing.Image)(resources.GetObject("box3.Image")));
+            this.box3.Location = new System.Drawing.Point(363, 112);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(60, 60);
+            this.box3.TabIndex = 2;
+            this.box3.UseVisualStyleBackColor = true;
+            // 
+            // box4
+            // 
+            this.box4.Image = ((System.Drawing.Image)(resources.GetObject("box4.Image")));
+            this.box4.Location = new System.Drawing.Point(429, 112);
+            this.box4.Name = "box4";
+            this.box4.Size = new System.Drawing.Size(60, 60);
+            this.box4.TabIndex = 3;
+            this.box4.UseVisualStyleBackColor = true;
+            // 
+            // box5
+            // 
+            this.box5.Image = ((System.Drawing.Image)(resources.GetObject("box5.Image")));
+            this.box5.Location = new System.Drawing.Point(495, 112);
+            this.box5.Name = "box5";
+            this.box5.Size = new System.Drawing.Size(60, 60);
+            this.box5.TabIndex = 4;
+            this.box5.UseVisualStyleBackColor = true;
+            // 
+            // box6
+            // 
+            this.box6.Image = ((System.Drawing.Image)(resources.GetObject("box6.Image")));
+            this.box6.Location = new System.Drawing.Point(231, 178);
+            this.box6.Name = "box6";
+            this.box6.Size = new System.Drawing.Size(60, 60);
+            this.box6.TabIndex = 5;
+            this.box6.UseVisualStyleBackColor = true;
+            // 
+            // box7
+            // 
+            this.box7.Image = ((System.Drawing.Image)(resources.GetObject("box7.Image")));
+            this.box7.Location = new System.Drawing.Point(297, 178);
+            this.box7.Name = "box7";
+            this.box7.Size = new System.Drawing.Size(60, 60);
+            this.box7.TabIndex = 6;
+            this.box7.UseVisualStyleBackColor = true;
+            // 
+            // box8
+            // 
+            this.box8.Image = ((System.Drawing.Image)(resources.GetObject("box8.Image")));
+            this.box8.Location = new System.Drawing.Point(363, 178);
+            this.box8.Name = "box8";
+            this.box8.Size = new System.Drawing.Size(60, 60);
+            this.box8.TabIndex = 7;
+            this.box8.UseVisualStyleBackColor = true;
+            // 
+            // box9
+            // 
+            this.box9.Image = ((System.Drawing.Image)(resources.GetObject("box9.Image")));
+            this.box9.Location = new System.Drawing.Point(429, 178);
+            this.box9.Name = "box9";
+            this.box9.Size = new System.Drawing.Size(60, 60);
+            this.box9.TabIndex = 8;
+            this.box9.UseVisualStyleBackColor = true;
+            // 
+            // box10
+            // 
+            this.box10.Image = ((System.Drawing.Image)(resources.GetObject("box10.Image")));
+            this.box10.Location = new System.Drawing.Point(495, 178);
+            this.box10.Name = "box10";
+            this.box10.Size = new System.Drawing.Size(60, 60);
+            this.box10.TabIndex = 9;
+            this.box10.UseVisualStyleBackColor = true;
+            // 
+            // box11
+            // 
+            this.box11.Image = ((System.Drawing.Image)(resources.GetObject("box11.Image")));
+            this.box11.Location = new System.Drawing.Point(231, 244);
+            this.box11.Name = "box11";
+            this.box11.Size = new System.Drawing.Size(60, 60);
+            this.box11.TabIndex = 10;
+            this.box11.UseVisualStyleBackColor = true;
+            // 
+            // box16
+            // 
+            this.box16.Image = ((System.Drawing.Image)(resources.GetObject("box16.Image")));
+            this.box16.Location = new System.Drawing.Point(231, 310);
+            this.box16.Name = "box16";
+            this.box16.Size = new System.Drawing.Size(60, 60);
+            this.box16.TabIndex = 11;
+            this.box16.UseVisualStyleBackColor = true;
+            // 
+            // box21
+            // 
+            this.box21.Image = ((System.Drawing.Image)(resources.GetObject("box21.Image")));
+            this.box21.Location = new System.Drawing.Point(231, 376);
+            this.box21.Name = "box21";
+            this.box21.Size = new System.Drawing.Size(60, 60);
+            this.box21.TabIndex = 12;
+            this.box21.UseVisualStyleBackColor = true;
+            // 
+            // box12
+            // 
+            this.box12.Image = ((System.Drawing.Image)(resources.GetObject("box12.Image")));
+            this.box12.Location = new System.Drawing.Point(297, 244);
+            this.box12.Name = "box12";
+            this.box12.Size = new System.Drawing.Size(60, 60);
+            this.box12.TabIndex = 13;
+            this.box12.UseVisualStyleBackColor = true;
+            // 
+            // box13
+            // 
+            this.box13.Image = ((System.Drawing.Image)(resources.GetObject("box13.Image")));
+            this.box13.Location = new System.Drawing.Point(363, 244);
+            this.box13.Name = "box13";
+            this.box13.Size = new System.Drawing.Size(60, 60);
+            this.box13.TabIndex = 14;
+            this.box13.UseVisualStyleBackColor = true;
+            // 
+            // box14
+            // 
+            this.box14.Image = ((System.Drawing.Image)(resources.GetObject("box14.Image")));
+            this.box14.Location = new System.Drawing.Point(429, 244);
+            this.box14.Name = "box14";
+            this.box14.Size = new System.Drawing.Size(60, 60);
+            this.box14.TabIndex = 15;
+            this.box14.UseVisualStyleBackColor = true;
+            // 
+            // box15
+            // 
+            this.box15.Image = ((System.Drawing.Image)(resources.GetObject("box15.Image")));
+            this.box15.Location = new System.Drawing.Point(495, 244);
+            this.box15.Name = "box15";
+            this.box15.Size = new System.Drawing.Size(60, 60);
+            this.box15.TabIndex = 16;
+            this.box15.UseVisualStyleBackColor = true;
+            // 
+            // box17
+            // 
+            this.box17.Image = ((System.Drawing.Image)(resources.GetObject("box17.Image")));
+            this.box17.Location = new System.Drawing.Point(297, 310);
+            this.box17.Name = "box17";
+            this.box17.Size = new System.Drawing.Size(60, 60);
+            this.box17.TabIndex = 17;
+            this.box17.UseVisualStyleBackColor = true;
+            // 
+            // box18
+            // 
+            this.box18.Image = ((System.Drawing.Image)(resources.GetObject("box18.Image")));
+            this.box18.Location = new System.Drawing.Point(363, 310);
+            this.box18.Name = "box18";
+            this.box18.Size = new System.Drawing.Size(60, 60);
+            this.box18.TabIndex = 18;
+            this.box18.UseVisualStyleBackColor = true;
+            // 
+            // box19
+            // 
+            this.box19.Image = ((System.Drawing.Image)(resources.GetObject("box19.Image")));
+            this.box19.Location = new System.Drawing.Point(429, 310);
+            this.box19.Name = "box19";
+            this.box19.Size = new System.Drawing.Size(60, 60);
+            this.box19.TabIndex = 19;
+            this.box19.UseVisualStyleBackColor = true;
+            // 
+            // box20
+            // 
+            this.box20.Image = ((System.Drawing.Image)(resources.GetObject("box20.Image")));
+            this.box20.Location = new System.Drawing.Point(495, 310);
+            this.box20.Name = "box20";
+            this.box20.Size = new System.Drawing.Size(60, 60);
+            this.box20.TabIndex = 20;
+            this.box20.UseVisualStyleBackColor = true;
+            // 
+            // box22
+            // 
+            this.box22.Image = ((System.Drawing.Image)(resources.GetObject("box22.Image")));
+            this.box22.Location = new System.Drawing.Point(297, 376);
+            this.box22.Name = "box22";
+            this.box22.Size = new System.Drawing.Size(60, 60);
+            this.box22.TabIndex = 21;
+            this.box22.UseVisualStyleBackColor = true;
+            // 
+            // box23
+            // 
+            this.box23.Image = ((System.Drawing.Image)(resources.GetObject("box23.Image")));
+            this.box23.Location = new System.Drawing.Point(363, 376);
+            this.box23.Name = "box23";
+            this.box23.Size = new System.Drawing.Size(60, 60);
+            this.box23.TabIndex = 22;
+            this.box23.UseVisualStyleBackColor = true;
+            // 
+            // box24
+            // 
+            this.box24.Image = ((System.Drawing.Image)(resources.GetObject("box24.Image")));
+            this.box24.Location = new System.Drawing.Point(429, 376);
+            this.box24.Name = "box24";
+            this.box24.Size = new System.Drawing.Size(60, 60);
+            this.box24.TabIndex = 23;
+            this.box24.UseVisualStyleBackColor = true;
+            // 
+            // box25
+            // 
+            this.box25.Image = ((System.Drawing.Image)(resources.GetObject("box25.Image")));
+            this.box25.Location = new System.Drawing.Point(495, 376);
+            this.box25.Name = "box25";
+            this.box25.Size = new System.Drawing.Size(60, 60);
+            this.box25.TabIndex = 24;
+            this.box25.UseVisualStyleBackColor = true;
+            // 
+            // header
+            // 
+            this.header.AutoSize = true;
+            this.header.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.header.ForeColor = System.Drawing.Color.Lime;
+            this.header.Location = new System.Drawing.Point(268, 42);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(252, 48);
+            this.header.TabIndex = 25;
+            this.header.Text = "Tic Tac Toe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 574);
+            this.Controls.Add(this.header);
+            this.Controls.Add(this.box25);
+            this.Controls.Add(this.box24);
+            this.Controls.Add(this.box23);
+            this.Controls.Add(this.box22);
+            this.Controls.Add(this.box20);
+            this.Controls.Add(this.box19);
+            this.Controls.Add(this.box18);
+            this.Controls.Add(this.box17);
+            this.Controls.Add(this.box15);
+            this.Controls.Add(this.box14);
+            this.Controls.Add(this.box13);
+            this.Controls.Add(this.box12);
+            this.Controls.Add(this.box21);
+            this.Controls.Add(this.box16);
+            this.Controls.Add(this.box11);
+            this.Controls.Add(this.box10);
+            this.Controls.Add(this.box9);
+            this.Controls.Add(this.box8);
+            this.Controls.Add(this.box7);
+            this.Controls.Add(this.box6);
+            this.Controls.Add(this.box5);
+            this.Controls.Add(this.box4);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box1);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button box1;
+        private System.Windows.Forms.Button box2;
+        private System.Windows.Forms.Button box3;
+        private System.Windows.Forms.Button box4;
+        private System.Windows.Forms.Button box5;
+        private System.Windows.Forms.Button box6;
+        private System.Windows.Forms.Button box7;
+        private System.Windows.Forms.Button box8;
+        private System.Windows.Forms.Button box9;
+        private System.Windows.Forms.Button box10;
+        private System.Windows.Forms.Button box11;
+        private System.Windows.Forms.Button box16;
+        private System.Windows.Forms.Button box21;
+        private System.Windows.Forms.Button box12;
+        private System.Windows.Forms.Button box13;
+        private System.Windows.Forms.Button box14;
+        private System.Windows.Forms.Button box15;
+        private System.Windows.Forms.Button box17;
+        private System.Windows.Forms.Button box18;
+        private System.Windows.Forms.Button box19;
+        private System.Windows.Forms.Button box20;
+        private System.Windows.Forms.Button box22;
+        private System.Windows.Forms.Button box23;
+        private System.Windows.Forms.Button box24;
+        private System.Windows.Forms.Button box25;
+        private System.Windows.Forms.Label header;
     }
 }
 
