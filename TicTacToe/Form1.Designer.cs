ac85d9688ff60f3f198ab500b08b14ac3cda60a9
Adam Wonia <adam.wonia@gmail.com>
2021-11-26 15:58:30 +0100

    reset function added and images resized

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -63,21 +63,24 @@ namespace TicTacToe
             this.label4 = new System.Windows.Forms.Label();
             this.pointsX = new System.Windows.Forms.Label();
             this.pointsO = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // box1
             // 
             this.box1.Image = ((System.Drawing.Image)(resources.GetObject("box1.Image")));
-            this.box1.Location = new System.Drawing.Point(231, 112);
+            this.box1.Location = new System.Drawing.Point(49, 112);
             this.box1.Name = "box1";
             this.box1.Size = new System.Drawing.Size(60, 60);
             this.box1.TabIndex = 0;
             this.box1.UseVisualStyleBackColor = true;
+            this.box1.Click += new System.EventHandler(this.box1_Click);
             // 
             // box2
             // 
             this.box2.Image = ((System.Drawing.Image)(resources.GetObject("box2.Image")));
-            this.box2.Location = new System.Drawing.Point(297, 112);
+            this.box2.Location = new System.Drawing.Point(115, 112);
             this.box2.Name = "box2";
             this.box2.Size = new System.Drawing.Size(60, 60);
             this.box2.TabIndex = 1;
@@ -86,7 +89,7 @@ namespace TicTacToe
             // box3
             // 
             this.box3.Image = ((System.Drawing.Image)(resources.GetObject("box3.Image")));
-            this.box3.Location = new System.Drawing.Point(363, 112);
+            this.box3.Location = new System.Drawing.Point(181, 112);
             this.box3.Name = "box3";
             this.box3.Size = new System.Drawing.Size(60, 60);
             this.box3.TabIndex = 2;
@@ -95,7 +98,7 @@ namespace TicTacToe
             // box4
             // 
             this.box4.Image = ((System.Drawing.Image)(resources.GetObject("box4.Image")));
-            this.box4.Location = new System.Drawing.Point(429, 112);
+            this.box4.Location = new System.Drawing.Point(247, 112);
             this.box4.Name = "box4";
             this.box4.Size = new System.Drawing.Size(60, 60);
             this.box4.TabIndex = 3;
@@ -104,7 +107,7 @@ namespace TicTacToe
             // box5
             // 
             this.box5.Image = ((System.Drawing.Image)(resources.GetObject("box5.Image")));
-            this.box5.Location = new System.Drawing.Point(495, 112);
+            this.box5.Location = new System.Drawing.Point(313, 112);
             this.box5.Name = "box5";
             this.box5.Size = new System.Drawing.Size(60, 60);
             this.box5.TabIndex = 4;
@@ -113,7 +116,7 @@ namespace TicTacToe
             // box6
             // 
             this.box6.Image = ((System.Drawing.Image)(resources.GetObject("box6.Image")));
-            this.box6.Location = new System.Drawing.Point(231, 178);
+            this.box6.Location = new System.Drawing.Point(49, 178);
             this.box6.Name = "box6";
             this.box6.Size = new System.Drawing.Size(60, 60);
             this.box6.TabIndex = 5;
@@ -122,7 +125,7 @@ namespace TicTacToe
             // box7
             // 
             this.box7.Image = ((System.Drawing.Image)(resources.GetObject("box7.Image")));
-            this.box7.Location = new System.Drawing.Point(297, 178);
+            this.box7.Location = new System.Drawing.Point(115, 178);
             this.box7.Name = "box7";
             this.box7.Size = new System.Drawing.Size(60, 60);
             this.box7.TabIndex = 6;
@@ -131,7 +134,7 @@ namespace TicTacToe
             // box8
             // 
             this.box8.Image = ((System.Drawing.Image)(resources.GetObject("box8.Image")));
-            this.box8.Location = new System.Drawing.Point(363, 178);
+            this.box8.Location = new System.Drawing.Point(181, 178);
             this.box8.Name = "box8";
             this.box8.Size = new System.Drawing.Size(60, 60);
             this.box8.TabIndex = 7;
@@ -140,7 +143,7 @@ namespace TicTacToe
             // box9
             // 
             this.box9.Image = ((System.Drawing.Image)(resources.GetObject("box9.Image")));
-            this.box9.Location = new System.Drawing.Point(429, 178);
+            this.box9.Location = new System.Drawing.Point(247, 178);
             this.box9.Name = "box9";
             this.box9.Size = new System.Drawing.Size(60, 60);
             this.box9.TabIndex = 8;
@@ -149,7 +152,7 @@ namespace TicTacToe
             // box10
             // 
             this.box10.Image = ((System.Drawing.Image)(resources.GetObject("box10.Image")));
-            this.box10.Location = new System.Drawing.Point(495, 178);
+            this.box10.Location = new System.Drawing.Point(313, 178);
             this.box10.Name = "box10";
             this.box10.Size = new System.Drawing.Size(60, 60);
             this.box10.TabIndex = 9;
@@ -158,7 +161,7 @@ namespace TicTacToe
             // box11
             // 
             this.box11.Image = ((System.Drawing.Image)(resources.GetObject("box11.Image")));
-            this.box11.Location = new System.Drawing.Point(231, 244);
+            this.box11.Location = new System.Drawing.Point(49, 244);
             this.box11.Name = "box11";
             this.box11.Size = new System.Drawing.Size(60, 60);
             this.box11.TabIndex = 10;
@@ -167,7 +170,7 @@ namespace TicTacToe
             // box16
             // 
             this.box16.Image = ((System.Drawing.Image)(resources.GetObject("box16.Image")));
-            this.box16.Location = new System.Drawing.Point(231, 310);
+            this.box16.Location = new System.Drawing.Point(49, 310);
             this.box16.Name = "box16";
             this.box16.Size = new System.Drawing.Size(60, 60);
             this.box16.TabIndex = 11;
@@ -176,7 +179,7 @@ namespace TicTacToe
             // box21
             // 
             this.box21.Image = ((System.Drawing.Image)(resources.GetObject("box21.Image")));
-            this.box21.Location = new System.Drawing.Point(231, 376);
+            this.box21.Location = new System.Drawing.Point(49, 376);
             this.box21.Name = "box21";
             this.box21.Size = new System.Drawing.Size(60, 60);
             this.box21.TabIndex = 12;
@@ -185,7 +188,7 @@ namespace TicTacToe
             // box12
             // 
             this.box12.Image = ((System.Drawing.Image)(resources.GetObject("box12.Image")));
-            this.box12.Location = new System.Drawing.Point(297, 244);
+            this.box12.Location = new System.Drawing.Point(115, 244);
             this.box12.Name = "box12";
             this.box12.Size = new System.Drawing.Size(60, 60);
             this.box12.TabIndex = 13;
@@ -194,7 +197,7 @@ namespace TicTacToe
             // box13
             // 
             this.box13.Image = ((System.Drawing.Image)(resources.GetObject("box13.Image")));
-            this.box13.Location = new System.Drawing.Point(363, 244);
+            this.box13.Location = new System.Drawing.Point(181, 244);
             this.box13.Name = "box13";
             this.box13.Size = new System.Drawing.Size(60, 60);
             this.box13.TabIndex = 14;
@@ -203,7 +206,7 @@ namespace TicTacToe
             // box14
             // 
             this.box14.Image = ((System.Drawing.Image)(resources.GetObject("box14.Image")));
-            this.box14.Location = new System.Drawing.Point(429, 244);
+            this.box14.Location = new System.Drawing.Point(247, 244);
             this.box14.Name = "box14";
             this.box14.Size = new System.Drawing.Size(60, 60);
             this.box14.TabIndex = 15;
@@ -212,7 +215,7 @@ namespace TicTacToe
             // box15
             // 
             this.box15.Image = ((System.Drawing.Image)(resources.GetObject("box15.Image")));
-            this.box15.Location = new System.Drawing.Point(495, 244);
+            this.box15.Location = new System.Drawing.Point(313, 244);
             this.box15.Name = "box15";
             this.box15.Size = new System.Drawing.Size(60, 60);
             this.box15.TabIndex = 16;
@@ -221,7 +224,7 @@ namespace TicTacToe
             // box17
             // 
             this.box17.Image = ((System.Drawing.Image)(resources.GetObject("box17.Image")));
-            this.box17.Location = new System.Drawing.Point(297, 310);
+            this.box17.Location = new System.Drawing.Point(115, 310);
             this.box17.Name = "box17";
             this.box17.Size = new System.Drawing.Size(60, 60);
             this.box17.TabIndex = 17;
@@ -230,7 +233,7 @@ namespace TicTacToe
             // box18
             // 
             this.box18.Image = ((System.Drawing.Image)(resources.GetObject("box18.Image")));
-            this.box18.Location = new System.Drawing.Point(363, 310);
+            this.box18.Location = new System.Drawing.Point(181, 310);
             this.box18.Name = "box18";
             this.box18.Size = new System.Drawing.Size(60, 60);
             this.box18.TabIndex = 18;
@@ -239,7 +242,7 @@ namespace TicTacToe
             // box19
             // 
             this.box19.Image = ((System.Drawing.Image)(resources.GetObject("box19.Image")));
-            this.box19.Location = new System.Drawing.Point(429, 310);
+            this.box19.Location = new System.Drawing.Point(247, 310);
             this.box19.Name = "box19";
             this.box19.Size = new System.Drawing.Size(60, 60);
             this.box19.TabIndex = 19;
@@ -248,7 +251,7 @@ namespace TicTacToe
             // box20
             // 
             this.box20.Image = ((System.Drawing.Image)(resources.GetObject("box20.Image")));
-            this.box20.Location = new System.Drawing.Point(495, 310);
+            this.box20.Location = new System.Drawing.Point(313, 310);
             this.box20.Name = "box20";
             this.box20.Size = new System.Drawing.Size(60, 60);
             this.box20.TabIndex = 20;
@@ -257,7 +260,7 @@ namespace TicTacToe
             // box22
             // 
             this.box22.Image = ((System.Drawing.Image)(resources.GetObject("box22.Image")));
-            this.box22.Location = new System.Drawing.Point(297, 376);
+            this.box22.Location = new System.Drawing.Point(115, 376);
             this.box22.Name = "box22";
             this.box22.Size = new System.Drawing.Size(60, 60);
             this.box22.TabIndex = 21;
@@ -266,7 +269,7 @@ namespace TicTacToe
             // box23
             // 
             this.box23.Image = ((System.Drawing.Image)(resources.GetObject("box23.Image")));
-            this.box23.Location = new System.Drawing.Point(363, 376);
+            this.box23.Location = new System.Drawing.Point(181, 376);
             this.box23.Name = "box23";
             this.box23.Size = new System.Drawing.Size(60, 60);
             this.box23.TabIndex = 22;
@@ -275,7 +278,7 @@ namespace TicTacToe
             // box24
             // 
             this.box24.Image = ((System.Drawing.Image)(resources.GetObject("box24.Image")));
-            this.box24.Location = new System.Drawing.Point(429, 376);
+            this.box24.Location = new System.Drawing.Point(247, 376);
             this.box24.Name = "box24";
             this.box24.Size = new System.Drawing.Size(60, 60);
             this.box24.TabIndex = 23;
@@ -284,7 +287,7 @@ namespace TicTacToe
             // box25
             // 
             this.box25.Image = ((System.Drawing.Image)(resources.GetObject("box25.Image")));
-            this.box25.Location = new System.Drawing.Point(495, 376);
+            this.box25.Location = new System.Drawing.Point(313, 376);
             this.box25.Name = "box25";
             this.box25.Size = new System.Drawing.Size(60, 60);
             this.box25.TabIndex = 24;
@@ -295,7 +298,7 @@ namespace TicTacToe
             this.header.AutoSize = true;
             this.header.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.header.ForeColor = System.Drawing.Color.Lime;
-            this.header.Location = new System.Drawing.Point(268, 42);
+            this.header.Location = new System.Drawing.Point(86, 42);
             this.header.Name = "header";
             this.header.Size = new System.Drawing.Size(252, 48);
             this.header.TabIndex = 25;
@@ -305,7 +308,8 @@ namespace TicTacToe
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(576, 255);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(414, 50);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(101, 39);
             this.label1.TabIndex = 26;
@@ -315,7 +319,8 @@ namespace TicTacToe
             // 
             this.turnLabel.AutoSize = true;
             this.turnLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.turnLabel.Location = new System.Drawing.Point(695, 255);
+            this.turnLabel.ForeColor = System.Drawing.Color.Red;
+            this.turnLabel.Location = new System.Drawing.Point(521, 50);
             this.turnLabel.Name = "turnLabel";
             this.turnLabel.Size = new System.Drawing.Size(41, 39);
             this.turnLabel.TabIndex = 27;
@@ -325,7 +330,7 @@ namespace TicTacToe
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(349, 460);
+            this.label2.Location = new System.Drawing.Point(167, 460);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 29);
             this.label2.TabIndex = 28;
@@ -335,7 +340,7 @@ namespace TicTacToe
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(252, 500);
+            this.label3.Location = new System.Drawing.Point(70, 500);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(39, 29);
             this.label3.TabIndex = 29;
@@ -345,7 +350,7 @@ namespace TicTacToe
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(448, 500);
+            this.label4.Location = new System.Drawing.Point(277, 500);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 29);
             this.label4.TabIndex = 30;
@@ -354,8 +359,10 @@ namespace TicTacToe
             // pointsX
             // 
             this.pointsX.AutoSize = true;
+            this.pointsX.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.pointsX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.pointsX.Location = new System.Drawing.Point(297, 500);
+            this.pointsX.ForeColor = System.Drawing.Color.Lime;
+            this.pointsX.Location = new System.Drawing.Point(115, 500);
             this.pointsX.Name = "pointsX";
             this.pointsX.Size = new System.Drawing.Size(27, 29);
             this.pointsX.TabIndex = 31;
@@ -365,18 +372,41 @@ namespace TicTacToe
             // 
             this.pointsO.AutoSize = true;
             this.pointsO.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.pointsO.Location = new System.Drawing.Point(495, 500);
+            this.pointsO.ForeColor = System.Drawing.Color.Lime;
+            this.pointsO.Location = new System.Drawing.Point(324, 500);
             this.pointsO.Name = "pointsO";
             this.pointsO.Size = new System.Drawing.Size(27, 29);
             this.pointsO.TabIndex = 32;
             this.pointsO.Text = "0";
             // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(392, 183);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(200, 50);
+            this.startBtn.TabIndex = 33;
+            this.startBtn.Text = "START GAME";
+            this.startBtn.UseVisualStyleBackColor = true;
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(392, 310);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(200, 50);
+            this.resetBtn.TabIndex = 34;
+            this.resetBtn.Text = "RESET GAME";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 574);
+            this.ClientSize = new System.Drawing.Size(626, 574);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.startBtn);
             this.Controls.Add(this.pointsO);
             this.Controls.Add(this.pointsX);
             this.Controls.Add(this.label4);
@@ -410,6 +440,9 @@ namespace TicTacToe
             this.Controls.Add(this.box3);
             this.Controls.Add(this.box2);
             this.Controls.Add(this.box1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "TicTacToe";
             this.ResumeLayout(false);
@@ -452,6 +485,8 @@ namespace TicTacToe
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label pointsX;
         private System.Windows.Forms.Label pointsO;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button resetBtn;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,9 +12,59 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        bool player = true;
+        int move = 0;
+        int[,] matrix = new int[5, 5];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void box1_Click(object sender, EventArgs e)
+        {
+            string path = player ? "../../images/imageX.png" : "../../images/imageO.png";
+            box1.Image = Image.FromFile(path);
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            player = true;
+            move = 0;
+            turnLabel.Text = "X";
+            pointsO.Text = "0";
+            pointsX.Text = "0";
+            Button[] boxList = new Button[25];
+            boxList[0] = box1;
+            boxList[1] = box2;
+            boxList[2] = box3;
+            boxList[3] = box4;
+            boxList[4] = box5;
+            boxList[5] = box6;
+            boxList[6] = box7;
+            boxList[7] = box8;
+            boxList[8] = box9;
+            boxList[9] = box10;
+            boxList[10] = box11;
+            boxList[11] = box12;
+            boxList[12] = box13;
+            boxList[13] = box14;
+            boxList[14] = box15;
+            boxList[15] = box16;
+            boxList[16] = box17;
+            boxList[17] = box18;
+            boxList[18] = box19;
+            boxList[19] = box20;
+            boxList[20] = box21;
+            boxList[21] = box22;
+            boxList[22] = box23;
+            boxList[23] = box24;
+            boxList[24] = box25;
+            foreach (Button btn in boxList)
+            {
+                btn.Enabled = true;
+                btn.Image = Image.FromFile("../../images/default.png");
+            }
         }
     }
 }
